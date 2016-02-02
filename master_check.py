b16077eda4a3a64ca6767fe135e26c330ef6e09e
lafeversa <lafeversa@gmail.com>
2016-02-01 18:52:24 -0800

    Changed the find_active_file fx to always generate the most recent list of active jobs. Updated the fx description accordingly. Added load_mids fx. Updated compare_checks fx to test both load_ids and load_mids fx's.

--- master_check.py
+++ master_check.py
@@ -5,16 +5,42 @@ from os import replace
 from active_check import run as active_run
 
 def compare_checks():
+    '''Compares data from active_file to data in master_file and removes duplications from active_file.
     '''
-    Compares data from active_file to data in master_file and removes duplications from active_file.
-    '''
+    ids = load_ids()
+    mids = load_mids()
     print('Run compare checks')
+    print(ids[0:3])
+    print('Accessed IDs correctly')
+    print('Now checking MIDs')
+    print(mids[0:3])
+    
+def load_mids(master_file='master_file.xlsx'):
+    '''Loads all the unique identifiers for posted jobs found in master_file.
+    '''
+    m_WB = pxl.load_workbook(master_file)
+    m_ST = m_WB.active
+    num_rows = len(m_ST.rows)
+    row_count = 1
+    col_count = 2
+    mids = []
+    
+    while row_count <= num_rows:
+        c_index = m_ST.cell(row = row_count, column = col_count)
+        c_val = c_index.value
+        if c_val != None:
+            mids.append(c_val)
+        row_count += 1
+        
+    return mids
 
 def load_ids(active_file='active_file.xlsx'):
+    '''Loads all the unique identifiers for active jobs found in active_file.
+    '''
     a_WB = pxl.load_workbook(active_file)
     a_ST = a_WB.active
     num_rows = len(a_ST.rows)
-    row_count = 1 
+    row_count = 2
     col_count = 2
     ids = []
     
@@ -24,20 +50,17 @@ def load_ids(active_file='active_file.xlsx'):
         if c_val != None:
             ids.append(c_val)
         row_count += 1
-    print(ids)
-    compare_checks()
+
+    return ids
     
 def find_active_file(active_file='active_file.xlsx'):
-    '''Checks that active_file exists and proceeds or creates active_file and proceeds.
+    '''Runs the active_check.py script to generate the active_file and proceeds.
     
     Args:
         active_file (str) = File name of active data from user feed. Should end with '.xlsx'. Defaults to 'active_file.xlsx'.
     '''
-    if exists(active_file):
-        load_ids()
-    else:
-        active_run()
-        load_ids()
+    active_run()
+    load_ids()
 
 def write_blank(fout='master_file.xlsx'):
     '''Writes blank file and saves it to 'fout'.
@@ -46,6 +69,13 @@ def write_blank(fout='master_file.xlsx'):
         fout (str): File name to save the blank Excel master_file as. Should end with '.xlsx'. Defaults to 'master_file.xlsx'.
     '''
     newWB = pxl.Workbook()
+    sheet = newWB.active
+
+    headers = ['Title', 'Link', 'Description', 'Pub Date']
+    
+    for h in range(len(headers)):
+        sheet.cell(row = 1, column = h + 1).value = headers[h]
+        
     newWB.save(fout)
 
 def master_check(master_file='master_file.xlsx'):
@@ -62,6 +92,7 @@ def master_check(master_file='master_file.xlsx'):
         
 def run():
     master_check()
+    compare_checks()
     
 if __name__ == '__main__':
     run()
